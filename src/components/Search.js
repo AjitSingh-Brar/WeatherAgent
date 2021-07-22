90578eb02e7ea579079e5b898341362444dff7da
Ajit Singh <68308589+AjitSingh-Brar@users.noreply.github.com>
2021-07-22 16:01:16 -0400

    CHNG: Changes on App.css and index.css

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Search.css";
+
+import SearchIcon from "@material-ui/icons/Search";
 
 const Search = () => {
-  return <div></div>;
+  return (
+    <div className="search">
+      <div className="search__leftContainer"></div>
+      <div className="search__rightContainer">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
