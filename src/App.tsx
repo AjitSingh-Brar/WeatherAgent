06c1a581fc4a3916d3929baa2b6c4679bd4615a8
Ajit Singh <68308589+AjitSingh-Brar@users.noreply.github.com>
2021-07-24 00:51:43 -0400

    CHNG: Favourites component changed

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import React from "react";
 // import logo from "./logo.svg";
 // import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Search from "./components/Search";
+import Favourites from "./components/Favourites";
 
 function App() {
   return (
     <div className="App">
-      <Search />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Search />
+          </Route>
+          <Route path="/favourites">
+            <Favourites />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,7 +1,164 @@
 import React from "react";
 import "./Favourites.css";
+import SearchIcon from "@material-ui/icons/Search";
 const Favourites = () => {
-  return <div></div>;
+  return (
+    <div className="favourites">
+      <div className="favourites__background">
+        <div className="favourites__top">
+          <div className="favourites__logo">
+            <img src="../logo/weather-logo.png" alt="Logo" />
+          </div>
+          <div className="favourites__searchIcon">
+            <SearchIcon />
+          </div>
+        </div>
+        <div className="favourites__bottom">
+          <h1>Favourites</h1>
+
+          <div className="favourites__containers">
+            <div className="favourites__cityTile">
+              <div className="top">
+                <div className="favourites__cityName">
+                  <h3>London</h3>
+                </div>
+                <div className="favourites__temperature">
+                  <span>15 &deg;</span>
+                  <img src="../logo/sun.png" alt="weather-types" />
+                </div>
+              </div>
+
+              <div className="favourites__tempDescription">
+                <div className="favourites__weatherType">
+                  <p>Type</p>
+                  <p>Clear</p>
+                </div>
+                <div className="favourites__maxTemperature">
+                  <p>Maximum Temperature</p>
+                  <p>28</p>
+                </div>
+                <div className="favourites__minTemperature">
+                  <p>Minimum Temperature</p>
+                  <p>28</p>
+                </div>
+                <div className="favourites__humidity">
+                  <p>Humidity</p>
+                  <p>28</p>
+                </div>
+              </div>
+              <div className="favourites__button">
+                <div className="favourites__removeButton">Remove</div>
+              </div>
+            </div>
+
+            {/* ------------------------------ */}
+            <div className="favourites__cityTile">
+              <div className="top">
+                <div className="favourites__cityName">
+                  <h3>London</h3>
+                </div>
+                <div className="favourites__temperature">
+                  <span>15 &deg;</span>
+                  <img src="../logo/sun.png" alt="weather-types" />
+                </div>
+              </div>
+
+              <div className="favourites__tempDescription">
+                <div className="favourites__weatherType">
+                  <p>Type</p>
+                  <p>Clear</p>
+                </div>
+                <div className="favourites__maxTemperature">
+                  <p>Maximum Temperature</p>
+                  <p>28</p>
+                </div>
+                <div className="favourites__minTemperature">
+                  <p>Minimum Temperature</p>
+                  <p>28</p>
+                </div>
+                <div className="favourites__humidity">
+                  <p>Humidity</p>
+                  <p>28</p>
+                </div>
+              </div>
+              <div className="favourites__button">
+                <div className="favourites__removeButton">Remove</div>
+              </div>
+            </div>
+
+            {/* ------------------------------ */}
+            <div className="favourites__cityTile">
+              <div className="top">
+                <div className="favourites__cityName">
+                  <h3>London</h3>
+                </div>
+                <div className="favourites__temperature">
+                  <span>15 &deg;</span>
+                  <img src="../logo/sun.png" alt="weather-types" />
+                </div>
+              </div>
+
+              <div className="favourites__tempDescription">
+                <div className="favourites__weatherType">
+                  <p>Type</p>
+                  <p>Clear</p>
+                </div>
+                <div className="favourites__maxTemperature">
+                  <p>Maximum Temperature</p>
+                  <p>28</p>
+                </div>
+                <div className="favourites__minTemperature">
+                  <p>Minimum Temperature</p>
+                  <p>28</p>
+                </div>
+                <div className="favourites__humidity">
+                  <p>Humidity</p>
+                  <p>28</p>
+                </div>
+              </div>
+              <div className="favourites__button">
+                <div className="favourites__removeButton">Remove</div>
+              </div>
+            </div>
+            {/* ------------------------------ */}
+            <div className="favourites__cityTile">
+              <div className="top">
+                <div className="favourites__cityName">
+                  <h3>London</h3>
+                </div>
+                <div className="favourites__temperature">
+                  <span>15 &deg;</span>
+                  <img src="../logo/sun.png" alt="weather-types" />
+                </div>
+              </div>
+
+              <div className="favourites__tempDescription">
+                <div className="favourites__weatherType">
+                  <p>Type</p>
+                  <p>Clear</p>
+                </div>
+                <div className="favourites__maxTemperature">
+                  <p>Maximum Temperature</p>
+                  <p>28</p>
+                </div>
+                <div className="favourites__minTemperature">
+                  <p>Minimum Temperature</p>
+                  <p>28</p>
+                </div>
+                <div className="favourites__humidity">
+                  <p>Humidity</p>
+                  <p>28</p>
+                </div>
+              </div>
+              <div className="favourites__button">
+                <div className="favourites__removeButton">Remove</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Favourites;
